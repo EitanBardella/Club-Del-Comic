c6acf2f36d0030ed269a0126db8acf57ad46ffbe
Eitan <xxeitanxx11@gmail.com>
2024-01-09 19:43:48 -0300

    ahora si

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import  ItemListContainer  from './components/ListContainer/ItemListContainer'
 import ItemDetailContainer from './components/DetailContainer/ItemDetailContainer'
 import "bootstrap/dist/css/bootstrap.min.css" 
 import {BrowserRouter, Route, Routes} from "react-router-dom"
-
+import Contactos from './components/Contactos/Contactos'
+import Faq from './components/Faq/Faq'
 
 //Componente Padre renderiza coBmponentes hijos
 function App() {
@@ -13,17 +14,25 @@ function App() {
   return (
     <>
 
-
+    <BrowserRouter>
       <NavBar/>
+
+
       <div className='app'>
-        <ItemListContainer/>
-        <ItemDetailContainer ItemId={2}/>
+        <Routes>
+          <Route path="/" element= {<ItemListContainer/>} />
+          <Route path="/productos/" element= {<ItemDetailContainer/>} />
+          <Route path="/item/:id" element= {<ItemDetailContainer  />} />
+          <Route path="/contactos" element= {<Contactos/>} />
+          <Route path="/faq" element= {<Faq/>} />
+        </Routes>
       </div>
       <Footer/>
 
-
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
+
